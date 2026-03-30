4bcc55a637b0fdaa496a4294bc6aacb405a16b7c
lucasmtdap-ai <lucasmtdap@gmail.com>
2026-03-30 09:06:27 -0300

    adiciona produtos

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const app = express();
 
 app.use(express.json());
 
+const produtos = [];
+
 app.get("/", (req, res) => {
   res.send("API rodando 🚀");
+});
+
+app.post("/produtos", (req, res) => {
+  const produto = req.body;
+  produtos.push(produto);
+  res.json({ mensagem: "Produto salvo com sucesso" });
+});
+
+app.get("/produtos", (req, res) => {
+  res.json(produtos);
 });
 
 app.listen(3000, () => {
