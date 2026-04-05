079f50ebb661b4226dfcd8fb79143f10860832ed
lucasmtdap-ai <lucasmtdap@gmail.com>
2026-04-05 16:05:51 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
