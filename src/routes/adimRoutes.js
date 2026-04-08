58d6b7b5fd6fc85e28078d20f09ccee6364295a4
Lucas P. Costa <lucasmtdap@gmail.com>
2026-04-07 23:09:32 -0300

    Create adimRoutes.js

---
+++ src/routes/adimRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { pool } from "../config/db.js";
+
+const router = express.Router();
+
+router.get("/set-admin", async (req, res) => {
+  try {
+    await pool.query(
+      `
+      UPDATE usuarios
+      SET role = 'admin'
+      WHERE email = 'lucasmtdap@gmail.com'
+      `
+    );
+
+    res.json({ ok: true, message: "Agora você é admin" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erro ao definir admin" });
+  }
+});
+
+export default router;
