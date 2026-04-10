8ac153d0ced9cefb3159e8cc430109068a8d4035
Lucas P. Costa <lucasmtdap@gmail.com>
2026-04-10 14:38:40 -0300

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,21 @@
 export function getUser() {
   try {
-    return JSON.parse(localStorage.getItem("user") || "{}");
+    return JSON.parse(sessionStorage.getItem("user") || "{}");
   } catch {
     return {};
   }
 }
 
+export function getToken() {
+  try {
+    return sessionStorage.getItem("token");
+  } catch {
+    return null;
+  }
+}
+
 export function logout() {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
+  sessionStorage.removeItem("token");
+  sessionStorage.removeItem("user");
   window.location.replace("/");
 }
