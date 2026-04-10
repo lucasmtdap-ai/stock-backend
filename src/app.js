02852659d2e1f40bed1fa351883aab70fb5cd3fb
Lucas P. Costa <lucasmtdap@gmail.com>
2026-04-10 12:46:25 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import salesRoutes from "./routes/salesRoutes.js";
@@ -13,6 +14,7 @@ import categoriasRoutes from "./routes/categoriasRoutes.js";
 import usuariosRoutes from "./routes/usuariosRoutes.js";
 import configuracoesRoutes from "./routes/configuracoesRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+
 import { initDb } from "./utils/initDb.js";
 
 dotenv.config();
